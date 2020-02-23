9f489a33bbf58ea8245b9655015250a5edc984e8
Rakibul Yeasin <rytotul@gmail.com>
2020-02-24 00:07:55 +0600

    Final step with sqlite3 :fire:

--- app.py
+++ app.py
@@ -7,7 +7,24 @@ from flask import Flask, jsonify, render_template
 sys.dont_write_bytecode = True
 
 # Project Specific modules
-from routes import *
+from models import Model
+
+# Init app
+app = Flask(__name__)
+db = Model("result_info.db")
+
+@app.route('/')
+def root():
+    # return str(db.fetch())
+    return render_template('index.html')
+
+@app.route("/<roll>")
+def search(roll):
+    result = json.dumps([dict(row) for row in db.fetch(roll)][0])
+    result = result[11:-2].replace('"', '')
+    result = result.replace(', ', "\n")
+    result = result.upper()
+    return jsonify({"messages": [{"text": result}]})
 
 # Run app
 if __name__ == "__main__":
