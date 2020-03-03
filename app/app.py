b1ef87c2e0888df1069211dfb82e7648e00df6f8
Rakibul Yeasin <rytotul@gmail.com>
2020-03-03 13:27:24 +0600

    Refactored Codebase :fire:

--- app/app.py
+++ app/app.py
@@ -1,14 +1,21 @@
+import os
 import sys
 from flask import Flask
 
 # Stop writing Object Code
 sys.dont_write_bytecode = True
 
-# Route
-from route import *
+# Project Specific modules
+import views
+from views import db
 
-# app Instance
+# app Initialization
 app = Flask(__name__)
+
+# Routes
+app.add_url_rule('/', view_func=views.index)
+app.add_url_rule('/result', view_func=views.result_fallback)
+app.add_url_rule('/result/<roll>', view_func=views.result)
 
 if __name__ == "__main__":
     # Debugging Enabled
