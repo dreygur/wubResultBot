4dd13fffaf4fce0464e31e0d89ff674478363d6c
Rakibul Yeasin <rytotul@gmail.com>
2020-03-12 20:27:20 +0600

    Added to Github Pages :fire:

---
+++ docs/js/main.js
@@ -0,0 +1,29 @@
+// var form = document.getElementById("form");
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+// });
+
+var submit = document.getElementById("submit");
+var body = document.getElementById("body");
+var search = document.getElementById("search");
+
+submit.onclick = function () {
+    var keyword = document.getElementById("search").value;
+    var url = "https://cors-anywhere.herokuapp.com/https://wubresult.herokuapp.com/web/";
+    fetch(url + search.value).then((res) => {
+        return res.json();
+    }).then((data) => {
+        // var result = JSON.parse(data);
+        // var result = JSON.stringify(data);
+        // body.innerText = data;
+        console.log(data);
+
+        Swal.fire({
+            title: 'Result',
+            html: data.replace(/\n/g, "<br />")
+        });
+
+    });
+    // console.log(keyword);
+};
