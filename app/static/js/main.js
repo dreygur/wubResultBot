22ec94db31bd5deb3d13c49eb5c211756dd29a9a
Rakibul Yeasin <rytotul@gmail.com>
2020-03-12 20:52:03 +0600

    Added to Github Pages :fire:

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -10,7 +10,7 @@ var search = document.getElementById("search");
 
 submit.onclick = function () {
     var keyword = document.getElementById("search").value;
-    var url = window.location.pathname + "/web/";
+    var url = "https://cors-anywhere.herokuapp.com/https://wubresult.herokuapp.com/web/";
     fetch(url + search.value).then((res) => {
         return res.json();
     }).then((data) => {
